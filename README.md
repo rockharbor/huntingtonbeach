5a0ee93ecc033e3e3141ebbf5c734831215e094f
Jeremy Harris <jharris@rockharbor.org>
2011-09-26 13:49:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RH Huntington Beach
+
+This is the WordPress child theme for RH Huntington Beach. It inherits most of its
+templates and functionality from the __ROCK__HARBOR theme base.
+
+See the base theme for more information.
---
+++ libs/child_theme.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * RH Huntington Beach theme
+ * 
+ * @package rockharbor
+ * @subpackage huntingtonbeach
+ */
+class ChildTheme extends RockharborThemeBase {
+		
+/**
+ * List of options for this theme
+ * 
+ * @var array
+ */
+	protected $themeOptions = array(
+		'slug' => 'huntington_beach',
+		'short_name' => 'Huntington Beach'
+	);
+}
